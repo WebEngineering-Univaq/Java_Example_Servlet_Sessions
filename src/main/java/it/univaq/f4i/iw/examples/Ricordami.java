79fa10a1586a5fad386f92a9acd59ab8fa8bd3f5
Giuseppe Della Penna <giuseppe.dellapenna@univaq.it>
2024-09-23 12:15:35 +0200

    JakartaEE converted version

--- src/main/java/it/univaq/f4i/iw/examples/Ricordami.java
+++ src/main/java/it/univaq/f4i/iw/examples/Ricordami.java
@@ -3,11 +3,11 @@ package it.univaq.f4i.iw.examples;
 import it.univaq.f4i.iw.framework.result.HTMLResult;
 import it.univaq.f4i.iw.framework.utils.ServletHelpers;
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  *
